9c4518e90a0747d479cc58ca8e2c3dad4490e5e7
abhinav <bhattabhinav@github.com>
2020-03-16 19:46:50 -0400

    updated controls

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidbody;
+    public float thrust = 100f;
+    public float rotate = 20f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         ProcessInput();
+        
     }
 
     private void ProcessInput()
@@ -22,14 +26,17 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             print("THRUST");
+            rigidbody.AddRelativeForce(new Vector3(0, thrust * Time.deltaTime, 0));
         }
         if (Input.GetKey(KeyCode.A))
         {
             print("ROTATE LEFT");
+            transform.Rotate(new Vector3(0, 0, rotate * Time.deltaTime));
         }
         else if (Input.GetKey(KeyCode.D))
         {
             print("ROTATE RIGHT");
+            transform.Rotate(new Vector3(0, 0, -rotate * Time.deltaTime));
         }
     }
 }
