9034e012edde8389f6680703fcab7251d2aa3538
abhinav <bhattabhinav@github.com>
2020-03-16 19:16:36 -0400

    basic controller added

---
+++ Assets/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("THRUST");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("ROTATE LEFT");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("ROTATE RIGHT");
+        }
+    }
+}
